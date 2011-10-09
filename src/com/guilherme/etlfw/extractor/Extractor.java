09da80e966680db3033c508020af63b80291c923
Guilherme Gonçalves <ggoncalves.kcond@gmail.com>
2011-10-08 23:26:26 -0300

    Criação da classe extrator

--- src/com/guilherme/etlfw/extractor/Extractor.java
+++ src/com/guilherme/etlfw/extractor/Extractor.java
@@ -1,5 +1,35 @@
 package com.guilherme.etlfw.extractor;
 
-public class Extractor {
+import com.guilherme.etlfw.exceptions.UnkownRegistryException;
+import com.guilherme.etlfw.mask.file.FileMask;
+import com.guilherme.etlfw.mask.file.XMLFileMask;
+import com.guilherme.etlfw.mask.registry.XMLRegistryMask;
+import com.guilherme.etlfw.xml.reader.XMLReader;
 
+public abstract class Extractor {
+	private XMLFileMask fileMask;
+	private XMLReader reader;
+	
+	public Extractor(String fileName) {
+		this.fileMask = (XMLFileMask) getFileMaskDesign();
+		reader = new XMLReader(fileName);
+	}
+	public XMLRegistryMask extractOne() throws UnkownRegistryException {
+		return fileMask.getRegistryWithValues(reader.getElement());
+	}
+	
+	public boolean next() {
+		if(!reader.next())
+			return true;
+		return false;
+	}
+	
+	public boolean previous() {
+		if(!reader.previous())
+			return true;
+		return false;
+	}
+	
+	public abstract FileMask<?> getFileMaskDesign();
+	
 }
--- test-unit/com/guilherme/etlfw/extractor/ExtractorTest.java
+++
@@ -1,9 +0,0 @@
-package com.guilherme.etlfw.extractor;
-
-import org.junit.Assert;
-
-public class ExtractorTest extends Assert{
-	
-//	@Test public void should 
-
-}
